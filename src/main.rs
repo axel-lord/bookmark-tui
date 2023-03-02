d5cbdbba2dcd8fa780f674bbdb71c7b5d336b9ae
Axel Lord <axel.lord2000@gmail.com>
2023-03-02 21:06:20 +0100

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use std::io::stdout;
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+};
+
+fn main() -> Result<()> {
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor,
+    )?;
+
+    stdout()
+        .execute(SetForegroundColor(Color::Yellow))?
+        .execute(SetBackgroundColor(Color::DarkRed))?
+        .execute(Print("Warning Text!"))?
+        .execute(ResetColor)?;
+
+    Ok(())
+}
